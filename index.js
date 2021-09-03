3f29ae7f82e840eca1aaeeefb9eefcdd4714593f
Loyal Chow <loyalchow@gmail.com>
2021-09-03 09:36:28 -0700

    retry on 503

--- index.js
+++ index.js
@@ -171,9 +171,10 @@ function _runTunnel(opts, cb) {
 				if (err) {
 					return cb(err);
 				}
-				var notReady = resp.statusCode === 500 && /panic/.test(body) ||
-					resp.statusCode === 502 && body.details &&
-						body.details.err === 'tunnel session not ready yet';
+				var notReady500 = resp.statusCode === 500 && /panic/.test(body)
+			  var notReady502 = resp.statusCode === 502 && body.details && body.details.err === 'tunnel session not ready yet';
+			  var notReady503 = resp.statusCode === 503 && body.details && body.details.err === 'a successful ngrok tunnel session has not yet been established';
+				var notReady = notReady500 || notReady502 || notReady503;
 
 				if (notReady) {
 					return retries-- ?
